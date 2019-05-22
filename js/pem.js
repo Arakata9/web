cd7a67e7a501846007d783c83462bda94547c730
Arakata9 <49929623+Arakata9@users.noreply.github.com>
2019-05-22 18:55:33 +0900

    web
    5/22

---
+++ js/pem.js
@@ -0,0 +1,37 @@
+
+//マウスオーバー時の処理pem
+function mouseOn(){
+  var obj = document.getElementById("img");
+  obj.src = "images/pem.gif";
+}
+
+//マウスアウト時の処理
+function mouseOff(){
+  var obj = document.getElementById("img");
+  obj.src = "images/pemf.png";
+}
+
+
+//マウスオーバー時の処理work
+function mouseOn2(){
+  var obj = document.getElementById("img2");
+  obj.src = "images/webUIbw.png";
+}
+
+//マウスアウト時の処理
+function mouseOff2(){
+  var obj = document.getElementById("img2");
+  obj.src = "images/work.png";
+}
+
+//マウスオーバー時の処理contact
+function mouseOn3(){
+  var obj = document.getElementById("img3");
+  obj.src = "images/contactbw.png";
+}
+
+//マウスアウト時の処理
+function mouseOff3(){
+  var obj = document.getElementById("img3");
+  obj.src = "images/contact.png";
+}
